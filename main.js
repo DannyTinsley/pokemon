2b1ac8bbec454edd98df5a4b1c38d0f8f9eefc17
Danielle Tinsley <dtinsley000@nyc.yearup.org>
2018-11-19 16:52:43 -0500

    onclick box appears

--- main.js
+++ main.js
@@ -1,17 +1,101 @@
 // rewrite code to inlclude object oriented method as well as javascript classes (constructor). This will help you to use only one function for all three pokemon.
 
+let mewBox= document.getElementById("mewBox");
+let mew1= document.getElementById("attack1");
 
-//Example:
-//
+let bulby= document.getElementById("attack3");
+let bulbBox= document.getElementById("bulbBox")
+bulby.addEventListener("click", bulbStats);
+bulby.addEventListener("click", appear2);
 
+let sq= document.getElementById("attack2");
+let squirtBox= document.getElementById("squirtBox");
+sq.addEventListener("click", squirtStats);
+sq.addEventListener("click", appear3);
+
+
+
+function appear1() {
+
+  if (mewBox.style.visibility== "hidden") {
+      mewBox.style.visibility = "visible";
+  } else {
+      mewBox.style.visibility = "hidden";
+  }
+}
+
+
+mew1.addEventListener("click", mewStats);
+mew1.addEventListener("click", appear1);
+
+
+
+class Pokemon {
+  constructor(name, hp, attack, defense, ability) {
+    this.name= name;
+    this.hp= hp;
+    this.attack= attack;
+    this.defense= defense;
+    this.ability= ability;
+  }
+  // stats for each pokemon
+
+
+  stats() {
+    let mewPara= document.createElement("li");
+    let box1= document.getElementById("listStat");
+    //innerHTML order matters; must be above appendChild
+    box1.innerHTML=""
+    box1.appendChild(mewPara);
+
+    //create a lot of breaks to seperate lines
+
+    let br= document.createElement("br");
+    let br2= document.createElement("br");
+    let br3= document.createElement("br");
+    let br4= document.createElement("br");
+
+
+
+
+    let mewsName= document.createTextNode("Name: "+ this.name);
+    let mewsHP= document.createTextNode("HP: " + this.hp);
+    let mewsAttack = document.createTextNode ("Attack: "+
+    this.attack);
+    let mewsDefense= document.createTextNode ("Defense: "+ this.defense);
+    let mewsAbility= document.createTextNode ("Ability: "+ this.ability);
+    mewPara.appendChild(mewsName);
+    mewPara.appendChild(br);
+    mewPara.appendChild(mewsHP);
+    mewPara.appendChild(br2);
+    mewPara.appendChild(mewsAttack);
+    mewPara.appendChild(br3);
+    mewPara.appendChild(mewsDefense);
+    mewPara.appendChild(br4);
+    mewPara.appendChild(mewsAbility);
+
+  }
+}
+
+class Trainer {
+  constructor (){
+  this.pokemon= []
+}
+  all (){
+
+  }
+  get(name){
+
+  }
+}
 
 
 // get stats for mew
 
 
-let mew1= document.getElementById("attack1");
 
-mew1.addEventListener("click", mewStats);
+
+
 
 
 function mewStats (){
@@ -20,27 +104,9 @@ function mewStats (){
     console.log(response.data);
     console.log(response.data.abilities[0].ability.name);
 
-    document.write("Speed: " +response.data.stats[0].base_stat);
-    //new lines
-    document.write("<br>");
-    document.write("<br>");
-    document.write("<br>");
+    let mewFinally= new Pokemon (response.data.name, response.data.stats[5].base_stat, response.data.stats[4].base_stat, response.data.stats[3].base_stat, response.data.abilities[0].ability.name);
 
-    document.write("Main Ability: " +response.data.abilities[0].ability.name);
-
-    //new lines
-    document.write("<br>");
-    document.write("<br>");
-    document.write("<br>");
-
-    document.write ("Moves: " + " (1) "+ response.data.moves[8].move.name);
-
-    document.write (" (2) " + response.data.moves[1].move.name);
-
-    document.write (" (3) " + response.data.moves[10].move.name);
-    // document.write()
-
-
+    mewFinally.stats();
 } )
 }
 
@@ -49,9 +115,6 @@ function mewStats (){
 
 //get stats for bulbasaur
 
-let bulby= document.getElementById("attack3");
-
-bulby.addEventListener("click", bulbStats);
 
 
 function bulbStats (){
@@ -60,82 +123,44 @@ function bulbStats (){
     console.log(response.data);
     console.log(response.data.abilities[0].ability.name);
 
-    document.write("Speed: " +response.data.stats[0].base_stat);
-    //new lines
-    document.write("<br>");
-    document.write("<br>");
-    document.write("<br>");
+    let bulbFinally= new Pokemon (response.data.name, response.data.stats[5].base_stat, response.data.stats[4].base_stat, response.data.stats[3].base_stat, response.data.abilities[0].ability.name);
 
-    document.write("Main Ability: " +response.data.abilities[0].ability.name);
-
-    //new lines
-    document.write("<br>");
-    document.write("<br>");
-    document.write("<br>");
-
-    document.write ("Moves: " + " (1) "+ response.data.moves[8].move.name);
-
-    document.write (" (2) " + response.data.moves[1].move.name);
-
-    document.write (" (3) " + response.data.moves[10].move.name);
-    // document.write()
-
-  showBulbStats();
-  //sanity check below because I need it
-  // document.write("Stats");
-})
+    bulbFinally.stats();
+} )
 }
 
+function appear2() {
 
-function showBulbStats () {
-    var bulbBox = document.getElementsByClassName("bulbBox");
-
-
-    // if (x.style.display=== "none") {
-    //     x.style.display = "block";
-    // } else {
-    //     x.style.display = "none";
-    // }
-    // document.display="inline-block";
+  if (mewBox.style.visibility== "hidden") {
+      mewBox.style.visibility = "visible";
+  } else {
+      mewBox.style.visibility = "hidden";
+  }
 }
-
 
 
 
 
 // get stats for squirtle
 
-let sq= document.getElementById("attack2");
 
-sq.addEventListener("click", squirtStats);
-
-
-function squirtStats () {
+function squirtStats (){
   axios.get ("http://fizal.me/pokeapi/api/v2/name/squirtle.json")
   .then(function (response) {
     console.log(response.data);
     console.log(response.data.abilities[0].ability.name);
 
-    document.write("Speed: " +response.data.stats[0].base_stat);
-    //new lines
-    document.write("<br>");
-    document.write("<br>");
-    document.write("<br>");
+    let bulbFinally= new Pokemon (response.data.name, response.data.stats[5].base_stat, response.data.stats[4].base_stat, response.data.stats[3].base_stat, response.data.abilities[0].ability.name);
 
-    document.write("Main Ability: " +response.data.abilities[0].ability.name);
-
-    //new lines
-    document.write("<br>");
-    document.write("<br>");
-    document.write("<br>");
-
-    document.write ("Moves: " + " (1) "+ response.data.moves[8].move.name);
-
-    document.write (" (2) " + response.data.moves[1].move.name);
-
-    document.write (" (3) " + response.data.moves[10].move.name);
-    // document.write()
-
-
+    bulbFinally.stats();
 } )
+}
+
+function appear3() {
+
+  if (mewBox.style.visibility== "hidden") {
+      mewBox.style.visibility = "visible";
+  } else {
+      mewBox.style.visibility = "hidden";
+  }
 }
